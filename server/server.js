f5bdc326935e5cbfe8e1dbeba0e0feb90d4d3dc6
GingerDragon7 <sunnybhadani898@gmail.com>
2024-01-16 19:09:19 +0530

    Commit Converse AI Project

---
+++ server/server.js
@@ -0,0 +1,50 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import OpenAI from "openai";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Hello from converseAI",
+  });
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `${prompt}`,
+        },
+      ],
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+
+    res.status(200).send({
+      bot: response.choices[0].message,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
+});
+
+app.listen(5000, () => {
+  console.log(`Server is running on port http://localhost:5000`);
+});
